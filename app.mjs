15bbb2e058bd8eb99d0b68e78b5f52919b1b8050
RRFIM <saswswap@gmail.com>
2025-11-25 13:17:08 +0100

    project modeule 4

--- app.mjs
+++ app.mjs
@@ -1,20 +1,129 @@
 import { loadSequelize } from "./database.mjs";
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
+import cookieParser from "cookie-parser";
+import jwt from "jsonwebtoken";
 
-/**
- * Point d'entrée de l'application
- * Vous déclarer ici les routes de votre API REST
- */
+function isLoggedInJWT(UserModel) {
+    return async (request, response, next) => {
+        const token = request.cookies.token;
+        if (!token) 
+        {
+            return response.status(401).json({ message: 'Unauthorized: no token provieded' });
+        }
+        try {
+            const decoded = jwt.verify(token, JWT_SECRET);
+            request.userId = decoded.userId;
+
+            request.user = await UserModel.findPk(request.userId);
+            if (!request.user) {
+                return response.status(401).json({ message: 'Unauthrized' });
+            }
+            next();
+        } catch (error) {
+            return response.status(500).json({ message: 'Invalid token' });
+        }
+    }
+}
+
+
 async function main() {
+
     try {
         const sequelize = await loadSequelize();
         const app = express();
+        const JWT_SECRET = "HND&*Wye8971h2yed687&^@(@&dghuy8i!@%$^&*(&^^#&_+^##*^^";
+        const UserModel = sequelize.models.User;
+        const PostModel = sequelize.models.Post;
+        const CommentModel = sequelize.models.Comment;
+        app.use(cookieParser());
+        app.use(express.json());
+
+        //POST
+        //AUTHENTIFICATION
+        app.post('/register', async (request, response) => {
+            try {
+                const Userdata = request.body;
+                if (Userdata.password != Userdata.verifiedPassword) {
+                    return response.status(400).json({ message: "Passwords do not match" });
+                }
+
+                const newUser = await UserModel.create({
+                    username: Userdata.username, 
+                    email: Userdata.email,
+                    password: Userdata.password
+                });
+                
+                if (!newUser.username || !newUser.email || !newUser.password)
+                {
+                    return response.status(400).json({ message: "Email, password and verifiedPassword are required" });
+                }
+
+                response.status(201).json({ message: "User registered successfully", userId: newUser.id });
+            } catch (error) {
+                response.status(500).json({ message: 'Error registering user', error: error.message });
+            }
+        });
+
+        app.post('/login', async (request, response) => {
+            try {
+                const LoginData = request.body;
+                if (!LoginData.email || !LoginData.password) {
+                    return response.status(400).json({ message: "Wrong password or email" });
+                }
+                const LoginUser = await UserModel.findOne({
+                    email: LoginData.email,
+                    password: LoginData.password
+                });
+                console.log(LoginUser);
+
+                if (!LoginUser) 
+                {
+                    return response.status(401).json({ message: "Invalid User" });
+                }
+
+                const token = jwt.sign({ userId: LoginUser.id }, JWT_SECRET, { expiresIn: '1h' });
+                response.cookie('token', token, { httpOnly: true });
+                response.status(201).json({ message: "User loged in successfully", userId: LoginUser.id });
+            } catch (error) {
+                response.status(500).json({ message: 'Error logging in', error: error.message });
+            }
+        });
+
+        app.post('/logout', (request, response) => {
+            response.clearCookie('token');
+            response.json({ message: 'Logout successful' });
+        });
+
+        app.use(isLoggedInJWT(UserModel));
 
 
+        //CREATION
+        app.post('/posts', async (request, response) => {
+            try {
+
+                const { title, content } = request.body;
+                const newPost = await PostModel.create({ title, content, UserId: req.userId });
+                if (!UserModel) 
+                {
+                    return response.status(401).json({ message: 'Unauthrized' });
+                }
+
+                if (!title || !content)
+                {
+                    return response.status(400).json({ message: "cannot leave the title or content empty" });
+                }
+            } catch (error) {
+                console.log(error)
+                response.status(500).json({ message: 'Error posting post', error: error.message });
+            }
+        });
 
 
+        //GET
+
+        //DELETE
 
         app.listen(3000, () => {
             console.log("Serveur démarré sur http://localhost:3000");
@@ -25,4 +134,5 @@ async function main() {
         console.error("Error de chargement de Sequelize:", error);
     }
 }
+
 main();
--- database.mjs
+++ database.mjs
@@ -1,19 +1,63 @@
+import { request, response } from "express";
 import { Sequelize, DataTypes } from "sequelize";
 
+
 /**
- * 
  * @returns {Promise<Sequelize>}
  */
 export async function loadSequelize() {
     try {
-        const sequelize = new Sequelize(/* ... */);
-        // ...
+        const login = {
+            database: "app-database",
+            username: "root",
+            password: "root"
+        };
+
+        const sequelize = new Sequelize("app-database", "root", "root", {
+            host: '127.0.0.1',
+            dialect: 'mysql'
+        });
+        
+         sequelize.define("User",{
+            username: DataTypes.STRING,
+            email: DataTypes.STRING,
+            password: DataTypes.STRING
+        });
+        
+        
+        sequelize.define("Post",{
+            username: DataTypes.STRING,
+            title: DataTypes.STRING,
+            content: DataTypes.STRING,
+            createdAt: DataTypes.DATE,
+            userId : DataTypes.INTEGER
+        });
+
+        sequelize.define("Comment",{
+            username: DataTypes.STRING,
+            content: DataTypes.STRING
+        });
+
+        const User = sequelize.models.User;
+        const Post = sequelize.models.Post;
+        const Comment = sequelize.models.Comment;
+        
+        //USER
+        User.hasMany(Post);
+        User.hasMany(Comment);
+
+        //POST
+        Post.belongsTo(User);
+        Post.hasMany(Comment);
+        
+        //COMMENT
+        Comment.belongsTo(Post);
+        
+        
+        await sequelize.sync();
         return sequelize;
     } catch (error) {
         console.error(error);
         throw Error("Échec du chargement de Sequelize");
     }
-
-    // ...
-
 }
